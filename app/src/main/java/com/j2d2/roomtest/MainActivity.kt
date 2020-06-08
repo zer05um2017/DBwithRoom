8d9d601c0f3c1003e46d83bc90a80f9c1d67576d
manson125 <manson125@outlook.kr>
2020-06-08 16:17:52 +0900

    added Date and Time picker dialog

--- app/src/main/java/com/j2d2/roomtest/MainActivity.kt
+++ app/src/main/java/com/j2d2/roomtest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.j2d2.roomtest
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +43,60 @@ class MainActivity : AppCompatActivity() {
                 appDatabase?.userDao()?.deleteAll()
             }
         }
+
+//        editTextDate.setOnFocusChangeListener { view, hasFocus ->
+//            if(hasFocus)
+//                Toast.makeText(this@MainActivity, "focused", Toast.LENGTH_SHORT).show()
+//            else
+//                Toast.makeText(this@MainActivity, "focuse lose", Toast.LENGTH_SHORT).show()
+//
+//        }
+
+        editTextDate.setOnTouchListener { _: View, event:MotionEvent ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+//                    Toast.makeText(this@MainActivity, "focused", Toast.LENGTH_SHORT).show()
+                    val cal = Calendar.getInstance()
+                    val y = cal.get(Calendar.YEAR)
+                    val m = cal.get(Calendar.MONTH)
+                    val d = cal.get(Calendar.DAY_OF_MONTH)
+
+
+                    val datepickerdialog:DatePickerDialog = DatePickerDialog(this@MainActivity, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+//                    Toast.makeText(this@MainActivity, "$dayOfMonth $monthOfYear, $year", Toast.LENGTH_SHORT).show()
+                        editTextDate.setText("$dayOfMonth $monthOfYear, $year")
+                        // Display Selected date in textbox
+//                        lblDate.setText("" + dayOfMonth + " " + MONTHS[monthOfYear] + ", " + year)
+                    }, y, m, d)
+
+                    datepickerdialog.show()
+                    false
+                }
+            }
+
+            true
+        }
+
+        editTextTime.setOnTouchListener { _: View, event:MotionEvent ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+//                    Toast.makeText(this@MainActivity, "focused", Toast.LENGTH_SHORT).show()
+                    val c:Calendar= Calendar.getInstance()
+                    val hh=c.get(Calendar.HOUR_OF_DAY)
+                    val mm=c.get(Calendar.MINUTE)
+                    val timePickerDialog:TimePickerDialog=TimePickerDialog(this@MainActivity,
+                        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                            editTextTime.setText( ""+hourOfDay + ":" + minute);
+                    },hh,mm,true)
+                    timePickerDialog.show()
+                    false
+                }
+            }
+
+            true
+        }
+
+
     }
 
     override fun onDestroy() {
